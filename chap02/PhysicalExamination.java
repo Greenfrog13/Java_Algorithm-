09c3fefddf7bb54d1f167951fea1c668ff886c1b
JiwonJeong <50576758+Greenfrog13@users.noreply.github.com>
2021-08-20 17:27:58 +0900

    실습 2-14
    실행 결과 
    📦신체검사 리스트📦
    이름                  키         시력 
    --------------------------
    박현규             162         0.3
    함진아             173         0.7
    최윤미             175         2.0
    홍연의             171          1.5
    이수진             168         0.4
    김영준             174          1.2
    박용규             169         0.8
    
    평균 키 : 170.3cm
    
    시력 분포
    0.0~ : 0명 
    0.1~ : 0명 
    0.2~ : 0명 
    0.3~ : 1명 
    0.4~ : 1명 
    0.5~ : 0명 
    ...이하 생략...

---
+++ chap02/PhysicalExamination.java
@@ -0,0 +1,63 @@
+package chap02;
+import java.util.Scanner;
+
+class PhysicalExamination {
+	static final int VMAX = 21;
+	
+	static class PhyscData {
+		String name;
+		int height;
+		double vision;
+		
+		PhyscData (String name, int height, double vision){
+			this.name = name;
+			this.height = height;
+			this.vision = vision;
+		}
+	}
+	
+	static double aveHeight (PhyscData[] dat) {
+		double sum=0;
+		for(int i=0;i<dat.length;i++)
+			sum += dat[i].height;
+		
+		return sum/dat.length;
+	} 
+	
+	static void distVision(PhyscData[] dat, int[] dist) {
+		int i=0;
+		
+		dist[i] = 0;
+		for(i=0;i<dat.length;i++) 
+			if (dat[i].vision >= 0.0 && dat[i].vision <= VMAX / 10.0)
+				dist[(int)(dat[i].vision * 10)]++;
+	}
+	
+	public static void main(String[] args){
+		Scanner stdIn = new Scanner(System.in);
+		
+		PhyscData[] x = {
+			new PhyscData("박현규", 162, 0.3),
+			new PhyscData("함진아", 173, 0.7),
+			new PhyscData("최윤미", 175, 2.0),
+			new PhyscData("홍연의", 171, 1,5),
+			new PhyscData("이수진", 168, 0.4),
+			new PhyscData("김영준", 174, 1.2),
+			new PhyscData("박용규", 169, 0.8),
+		};
+		int[] vdist = new int[VMAX];
+		
+		System.out.println("📦신체검사 리스트📦");
+		System.out.println("이름      키  시력");
+		System.out.println("------------------");
+		for(int i=0;i<x.length;i++)
+			System.out.printf("%-8s%3d%5.1f\n", x[i].name, x[i].height, x[i].vision);
+		System.out.printf("\n평균 키 : %5.1fcm\n", aveHeight(x));
+		
+		distVision(x, vidst);
+		
+		System.out.println("\n시력 분포");
+		for(int i=0;i<VMAX;i++)
+			System.out.printf("%3.1f ~ : %2d명\n", i/10.0, vdist[i]);
+	}
+}
